5c6ca9fe6deac61f4f60f650bf215312a31894d9
Dmytro_Dolotov <Dmytro_Dolotov@epam.com>
2014-04-08 12:43:32 +0300

    Task 2 - commit

---
+++ hybristask2/src/de/hybris/tutorial/task2/constants/Hybristask2Constants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package de.hybris.tutorial.task2.constants;
+
+/**
+ * Global class for all Hybristask2 constants. You can add global constants for your extension into this class.
+ */
+public final class Hybristask2Constants extends GeneratedHybristask2Constants
+{
+	public static final String EXTENSIONNAME = "hybristask2";
+
+	private Hybristask2Constants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ hybristask2/web/webroot/index.jsp
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1>hybristask2</h1>
+		Welcome to my extension.
+	</body>
+</html>
+
